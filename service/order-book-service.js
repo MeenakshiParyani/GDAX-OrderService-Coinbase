5a18c2890ce31f4c20ea46e19ef3b91ac2409f18
meenakshi.paryani@sjsu.edu <meenakshi.paryani@sjsu.edu>
2018-01-18 20:27:45 -0800

    Adding Order Book Service

---
+++ service/order-book-service.js
@@ -0,0 +1,48 @@
+var https = require('https');
+var request = require("request");
+
+function getTradeStatusFromOrderBookService(currency1, currency2){
+  let url = 'https://api.gdax.com/products/' + currency1 + '-' + currency2 + '/book';
+  let options = {
+        url: url,
+        headers: {
+            'User-Agent': 'request'
+        }
+    };
+
+
+  console.log(url);
+    return new Promise(function(resolve, reject) {
+        console.log('Inside Promise');
+        request.get(options , function(err, res, body) {
+            if(res.statusCode==400){
+              console.log('Status: ' + res.statusCode);
+                console.log('Error is ');
+                console.log(err);
+                console.log(res);
+                console.log(body);
+                reject(err);
+            }else{
+                console.log('Status: ' + res.statusCode);
+                console.log(body);
+                resolve(res);
+            }
+            // res.on('data' , (data) => {
+            //     console.log('Status: ' + res.statusCode);
+            //     console.log(res);
+            //     resolve(JSON.parse(res.body));
+            // });
+            //
+            // res.on('error', (err) => {
+            //     console.log('Error is ');
+            //     console.log(err);
+            //     reject(err);
+            // });
+        });
+
+
+    });
+
+}
+
+exports.getTradeStatus = getTradeStatusFromOrderBookService;

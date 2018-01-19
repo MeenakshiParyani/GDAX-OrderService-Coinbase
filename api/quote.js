6750a5445ba53ab261894d9dc5a75087c8e4e4b5
meenakshi.paryani@sjsu.edu <meenakshi.paryani@sjsu.edu>
2018-01-19 13:33:38 -0800

    Sell Currency Workflow added

--- api/quote.js
+++ api/quote.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var orderbookService = require('../service/order-book-service.js');
 var bids = [];
 var asks = [];
+var currencyReversed = false;
 
 router.post('/', function(req,res){
   // Take the API input
@@ -17,23 +18,12 @@ router.post('/', function(req,res){
       // If Orderbook is present
       if(status){
           switch(action){
-             case 'buy'  : getQuotesFromAsks(asks, amount, quoteCurrency)
-                           .then(function(response){
-                              res.json(response);
-                           })
-                           .catch(function(err){
-                              res.json(err);
-                           });
+            //** for ex - buying USD equivalent to selling BTC as orderbook has BTC values only(only for one currency at a time)
+             case 'buy'  : currencyReversed ? sellCurrency(bids, amount, quoteCurrency, res) : buyCurrency(asks, amount, quoteCurrency, res);
                            break;
-             case 'sell' : getQuotesFromBids(bids, amount, quoteCurrency)
-                           .then(function(response){
-                              res.json(response);
-                           })
-                           .catch(function(err){
-                              res.json(err);
-                           });
+             case 'sell' : currencyReversed ? buyCurrency(asks, amount, quoteCurrency, res)  : sellCurrency(bids, amount, quoteCurrency, res);
                            break;
-             default     : res.json({"error" : "Invalid action, Please choose from buy/sell!!"});
+             default     : res.status(401).send({"error" : "Invalid action, Please choose from buy/sell!!"});
 
           }
       }else{//If Orderbook is not present
@@ -43,34 +33,93 @@ router.post('/', function(req,res){
 
 });
 
+function buyCurrency(asks, amount, quoteCurrency, res){
+    getQuotesFromAsks(asks, amount, quoteCurrency)
+    .then(function(response){
+       res.json(response);
+    })
+    .catch(function(err){
+       res.json(err);
+    });
+}
+
+function sellCurrency(bids, amount, quoteCurrency, res){
+    getQuotesFromBids(bids, amount, quoteCurrency)
+    .then(function(response){
+       res.json(response);
+    })
+    .catch(function(err){
+       res.json(err);
+    });
+}
+
 function getQuotesFromBids(bids, amount, quoteCurrency){
-    bids = sortByPrice(bids);
-    return bids;
+  return new Promise(function(resolve, reject) {
+      if(bids && bids.length){
+        console.log('before' + bids[0]);
+        bids = sortByPrice(bids, false); // If asks sort by ascending, if bids sort by descending
+        console.log('after' + bids[0]);
+        var updatedAmt = amount;
+        var i=0;
+        var pricePerUnitFinal = 0;
+        var totalPrice = 0;
+        while(updatedAmt > 0 && i <bids.length){
+          var perUnitPrice = asks[i][0];
+          var size = asks[i][1];
+            if(updatedAmt - (size * perUnitPrice) <=0){
+                totalPrice = totalPrice + (updatedAmt / (perUnitPrice * size));
+                console.log('total if ' + totalPrice + ' ' + updatedAmt + ' ' + perUnitPrice);
+                updatedAmt = 0;
+            }else{
+                totalPrice = totalPrice + size;
+                updatedAmt = updatedAmt - (size * perUnitPrice);
+                console.log('total else ' + totalPrice + ' ' + perUnitPrice + ' ' + size + ' ' + updatedAmt);
+            }
+            i++;
+        }
+        // Check if the bids could not meet the requirement
+        if(updatedAmt > 0){
+            reject({"error" : "Quantity not available, try with lower quantity!!"});
+        }else{
+          // Requirement was met
+          pricePerUnitFinal = totalPrice / amount;
+          var quote = {
+              "total"    : totalPrice,
+              "price"    : pricePerUnitFinal,
+              "currency" : quoteCurrency
+          }
+          resolve(quote);
+        }
+
+      }else{
+          reject({"error" : "Nothing available to buy at the moment!!"});
+      }
+  });
 }
 
 function getQuotesFromAsks(asks, amount, quoteCurrency){
   return new Promise(function(resolve, reject) {
       if(asks && asks.length){
         console.log('before' + asks[0]);
-        asks = sortByPrice(asks);
+        asks = sortByPrice(asks, true); // If asks sort by ascending, if bids sort by descending
         console.log('after' + asks[0]);
-        let updatedAmt = amount;
-        let i=0;
-        let pricePerUnitFinal = 0;
+        var updatedAmt = amount;
+        var i=0;
+        var pricePerUnitFinal = 0;
         var totalPrice = 0;
         while(updatedAmt > 0 && i <asks.length){
-            let perUnitPrice = asks[i][0];
-            let size = asks[i][1];
+            var perUnitPrice = asks[i][0];
+            var size = asks[i][1];
             console.log(perUnitPrice + ' ' + size);
             console.log(i);
             if((updatedAmt - size) <= 0){
-                console.log('total if ' + totalPrice + ' ' + updatedAmt + ' ' + perUnitPrice);
                 totalPrice = totalPrice + (updatedAmt * perUnitPrice);
                 updatedAmt = 0;
+                console.log('total if ' + totalPrice + ' ' + updatedAmt + ' ' + perUnitPrice);
             }else{
-                console.log('total else ' + totalPrice + ' ' + (size));
                 totalPrice = totalPrice + (size  * perUnitPrice);
                 updatedAmt = updatedAmt - size ;
+                console.log('total else ' + totalPrice + ' ' + (size));
             }
             i++;
         }
@@ -80,7 +129,7 @@ function getQuotesFromAsks(asks, amount, quoteCurrency){
         }else{
           // Requirement was met
           pricePerUnitFinal = totalPrice / amount;
-          let quote = {
+          var quote = {
               "total"    : totalPrice,
               "price"    : pricePerUnitFinal,
               "currency" : quoteCurrency
@@ -96,15 +145,27 @@ function getQuotesFromAsks(asks, amount, quoteCurrency){
 }
 
 // Sort the bids and asks by price which is the first value of the array
-function sortByPrice(items){
+function sortByPrice(items, sortAscending){
+  if(sortAscending){
+      items.sort(function (itemA, itemB) {
+        if (itemA[0] === itemB[0]) {
+            return 0;
+        }
+        else {
+            return (itemA[0] < itemB[0]) ? -1 : 1;
+        }
+    });
+  }else{
     items.sort(function (itemA, itemB) {
       if (itemA[0] === itemB[0]) {
           return 0;
       }
       else {
-          return (itemA[0] < itemB[0]) ? -1 : 1;
+          return (itemB[0] < itemA[0]) ? -1 : 1;
       }
     });
+  }
+
     console.log(items);
     return items;
 }
@@ -125,8 +186,11 @@ function isTradePossible(baseCurrency, quoteCurrency){
         // If first Combination didnt work, try reverse Combination
         return orderbookService.getTradeStatus(quoteCurrency, baseCurrency).then(function(data){
           if(data.statusCode == 200){
+            // If the currencies are reversed, exchange bids with asks,
+            //** buying USD equivalent to selling BTC, so now we need to see bids instead of asks
+            currencyReversed = true;
             bids = JSON.parse(data.body).bids;
-            asks = JSON.parse(data.body).asks;// TODO - see reverse calculation
+            asks = JSON.parse(data.body).asks;
             return true;
           }else{
             return false;
--- service/order-book-service.js
+++ service/order-book-service.js
@@ -20,9 +20,9 @@ function getTradeStatusFromOrderBookService(currency1, currency2){
             if(res.statusCode==400){
               console.log('Status: ' + res.statusCode);
                 console.log('Error is ');
-                console.log(err);//
-                console.log(res);//
-                console.log(body);//
+                //console.log(err);
+                //console.log(res);
+                //console.log(body);
                 reject(err);
             }else{
                 console.log('Status: ' + res.statusCode);
